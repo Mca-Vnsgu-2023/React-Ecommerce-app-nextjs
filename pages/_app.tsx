a1be665c13efde197491e2c7f6c1d253e5e3f218
Dinky <mcavnsgu2023@gmail.com>
2023-05-26 18:31:15 +0530

    changes the bootstrap cdn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,8 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux';
 import { Store } from '../Store/Store'
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-
-useEffect(()=>{
-  require("bootstrap/dist/js/bootstrap.bundle.min.js");
-},[])
 
   return (
     <Provider store={Store}>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import { ToastContainer } from 'react-toastify';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" /> </Head>
       <body>
         <Main />
         <ToastContainer />
